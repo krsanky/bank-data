7ddd9ceb7a3f0985e391fbf5a99e5fcfe861b916
paul wisehart <paul@oldcode.org>
2017-04-07 23:31:47 -0400

    lein run works

---
+++ src/bank_data/core.clj
@@ -0,0 +1,12 @@
+(ns bank-data.core (:gen-class))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+;;(defn -main [& args]
+(defn -main []
+  (println  "Hello1, World!"))
+
